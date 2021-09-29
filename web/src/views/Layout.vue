c63b2347d7e238b986586de560c85a4e60547f56
zerotower69 <zerotower@163.com>
2021-09-29 17:36:56 +0800

    footer 固定在底部

--- web/src/views/Layout.vue
+++ web/src/views/Layout.vue
@@ -1,11 +1,13 @@
 <template>
-<div>
-  <div class="app-header">
+<div class="app-wrapper">
+  <div :style="{minHeight:mainMinHeight+'px'}">
+    <div class="app-header">
     <Header></Header>
+    </div>
+      <router-view/>
   </div>
-  <router-view/>
   <div class="app-footer">
-    <Footer></Footer>
+    <Footer ref="footer"></Footer>
   </div>
 </div>
 </template>
@@ -15,10 +17,24 @@ import Header from '../components/public/Header'
 import Footer from '../components/public/Footer'
 export default {
   name: 'Layout',
-  components: { Footer, Header }
+  components: { Footer, Header },
+  computed: {
+    mainMinHeight () {
+      return window.innerHeight - this.$refs.footer.$el.offsetHeight
+    }
+  }
+
 }
 </script>
 
-<style scoped>
-
+<style lang="less" scoped>
+.app-wrapper{
+  width: 100%;
+  min-height: 100%;
+  //position: relative;
+  .app-footer{
+    bottom: 0;
+    width: 100%;
+  }
+}
 </style>
